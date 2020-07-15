62feed7ede2b990da56861bbd4e37c3647ddeb4a
Peter Shevchuk <raf02041994@gmail.com>
2020-07-15 22:49:01 +0300

    edit

--- module1/js/app.js
+++ module1/js/app.js
@@ -40,13 +40,37 @@ const task2 = {
   btnClick: classTasks[2].querySelector("[type='button']"),
   inputCheck: classTasks[2].querySelector("[type='number']"),
   text: ["Вітаю ви вгадали число ", "Ви програли, компютер загадав "],
-  numberComp: Math.floor(Math.random() * Math.floor(10)),
-  check() {
+  checkNumber() {
+    this.createNewNumber();
     this.number = Number(this.inputCheck.value);
-    alert(this.number === this.numberComp? this.text[0]+this.numberComp : this.text[1]+this.numberComp);
+    alert(this.number === this.numberComp ? this.text[0] + this.numberComp : this.text[1] + this.numberComp);
+  },
+  createNewNumber() {
+    this.numberComp = Math.floor(Math.random() * Math.floor(10));
   },
 };
 
 task2.btnClick.addEventListener("click", () => {
-  task2.check();
+  task2.checkNumber();
+});
+
+const task3 = {
+  btnClick: classTasks[3].querySelector("[type='button']"),
+  inputCheck: classTasks[3].querySelector("[name='react']"),
+  firstNumber: classTasks[3].querySelector("#firstNumber"),
+  secondNumber: classTasks[3].querySelector("#secondNumber"),
+  text: ["Сума", "Різниця", "Добуток", "Частка"],
+
+  calculator() {
+    classTasks[3].querySelector(".result").textContent = `${this.text[this.inputCheck.value]} чисел a i b = ${this.react(this.firstNumber.value, this.secondNumber.value, this.inputCheck.value)}`;
+  },
+  react(a, b, c) {
+    a = Number(a);
+    b = Number(b);
+    return c === "0" ? a + b : c === "1" ? a - b : c === "2" ? a * b : a / b;
+  },
+};
+
+task3.btnClick.addEventListener("click", () => {
+  task3.calculator();
 });
