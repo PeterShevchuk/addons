49777f464631310916075798b6f5304f4f400e8d
Peter Shevchuk <raf02041994@gmail.com>
2020-07-16 16:25:49 +0300

    add task 3-7

--- module1/js/app.js
+++ module1/js/app.js
@@ -13,19 +13,22 @@ const classTasks = [
   document.querySelector(".task9"),
 ];
 
+const result = (task, message) => {
+  let createText = document.createElement("p");
+  createText.append(message);
+  classTasks[task].append(createText);
+}
+
 const task1 = {
   btnClick: classTasks[1].querySelector("[type='button']"),
   inputCheck: classTasks[1].querySelector("[type='year']"),
   intercalary: ["Високосний", "Не високосний"],
   years: 0,
-  createText: "",
 
   check() {
     this.years = this.inputCheck.value;
     if (Boolean(this.years)) {
-      this.createText = document.createElement("p");
-      this.createText.append(`${this.years} - ${new Date(this.years, 1, 29).getMonth() === 1 ? this.intercalary[0] : this.intercalary[1]}`);
-      classTasks[1].append(this.createText);
+      result(1, `${this.years} - ${new Date(this.years, 1, 29).getMonth() === 1 ? this.intercalary[0] : this.intercalary[1]}`);
     } else {
       alert("Введено не вірно!");
     }
@@ -40,7 +43,7 @@ const task2 = {
   btnClick: classTasks[2].querySelector("[type='button']"),
   inputCheck: classTasks[2].querySelector("[type='number']"),
   text: ["Вітаю ви вгадали число ", "Ви програли, компютер загадав "],
-  checkNumber() {
+  check() {
     this.createNewNumber();
     this.number = Number(this.inputCheck.value);
     alert(this.number === this.numberComp ? this.text[0] + this.numberComp : this.text[1] + this.numberComp);
@@ -51,7 +54,7 @@ const task2 = {
 };
 
 task2.btnClick.addEventListener("click", () => {
-  task2.checkNumber();
+  task2.check();
 });
 
 const task3 = {
@@ -61,8 +64,8 @@ const task3 = {
   secondNumber: classTasks[3].querySelector("#secondNumber"),
   text: ["Сума", "Різниця", "Добуток", "Частка"],
 
-  calculator() {
-    classTasks[3].querySelector(".result").textContent = `${this.text[this.inputCheck.value]} чисел a i b = ${this.react(this.firstNumber.value, this.secondNumber.value, this.inputCheck.value)}`;
+  check() {
+    result(3, `${this.text[this.inputCheck.value]} чисел ${this.firstNumber.value} i ${this.secondNumber.value} = ${this.react(this.firstNumber.value, this.secondNumber.value, this.inputCheck.value)}`)
   },
   react(a, b, c) {
     a = Number(a);
@@ -72,5 +75,72 @@ const task3 = {
 };
 
 task3.btnClick.addEventListener("click", () => {
-  task3.calculator();
+  task3.check();
+});
+
+const task4 = {
+  btnClick: classTasks[4].querySelector("[type='button']"),
+  inputCheck: classTasks[4].querySelector("[type='number']"),
+  inputCheckTwo: classTasks[4].querySelector("[name='react']"),
+  check() {
+    this.inputCheckTwo.value === "0"? result(4,  `${this.inputCheck.value}°C цельсія дорівнює ${this.inputCheck.value * 1.8 + 32}°F фаренгейта`) : result(4, `${this.inputCheck.value}°F фаренгейта дорівнює  ${this.inputCheck.value - 32 / 1.8}°C цельсія`);
+  },
+};
+
+task4.btnClick.addEventListener("click", () => {
+  task4.check();
+});
+
+const task5 = {
+  btnClick: classTasks[5].querySelector("[type='button']"),
+  inputCheck: classTasks[5].querySelector("[type='number']"),
+  check() {
+    this.inputCheck.value >= 55 && this.inputCheck.value <= 99
+      ? result(5, ` Число ${this.inputCheck.value} входить в діапазон від 55 до 99`)
+      : result(5, ` Число ${this.inputCheck.value} не входить в діапазон від 55 до 99`);
+  },
+};
+
+task5.btnClick.addEventListener("click", () => {
+  task5.check();
+});
+
+const task6 = {
+  btnClick: classTasks[6].querySelector("[type='button']"),
+  inputCheck: [classTasks[6].querySelector("[name='input1']"), classTasks[6].querySelector("[name='input2']"), classTasks[6].querySelector("[name='input3']")],
+  check() {
+    let inputCheckVal = [this.inputCheck[0].value, this.inputCheck[1].value, this.inputCheck[2].value]
+    this.maxNumber = inputCheckVal[0];
+    if (Number(inputCheckVal[0]) && Number(inputCheckVal[1]) && Number(inputCheckVal[2])) {
+      for (const item of inputCheckVal) {
+        item > this.maxNumber? this.maxNumber =  item : null
+      }
+      result(6, `Найбільше число: ${this.maxNumber}`);
+    } else {
+      alert('Введіть число!')
+    }
+  }
+}
+task6.btnClick.addEventListener("click", () => {
+  task6.check();
+});
+
+const task7 = {
+  btnClick: classTasks[7].querySelector("[type='button']"),
+  inputCheck: classTasks[7].querySelector("[type='number']"),
+  check() {
+    this.rating = Number(this.inputCheck.value);
+    if (this.rating > 100) {alert('оцінка не може бути більшою ніж 100'); return;}
+    if (this.rating >= 89 && this.rating <= 100) {this.mess = 'A'}
+    if (this.rating < 89) {this.mess = 'B'}
+    if (this.rating < 75) {this.mess = 'C'}
+    if (this.rating < 60) {this.mess = 'D'}
+    if (this.rating < 45) {this.mess = 'E'}
+    if (this.rating < 20) {this.mess = 'F'}
+
+    result(7, `Оцінка студента: ${this.mess}`);
+  }
+}
+task7.btnClick.addEventListener("click", () => {
+  task7.check();
 });
